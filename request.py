282747dbe71208390d4951cb3bd399c21333234d
Andrew Beers <apbeers@gmail.com>
2019-03-10 21:06:15 -0500

    update url

--- request.py
+++ request.py
@@ -3,7 +3,7 @@
 import requests
 
 # URL
-url = 'https://nameless-island-82550.herokuapp.com/api'
+url = 'https://fathomless-thicket-49759.herokuapp.com//api'
 
 # Change the value of experience that you want to test
 r = requests.post(url, json={'url':'www.gooogle.com', })
