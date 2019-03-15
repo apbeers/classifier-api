a1632be25f0286321a794c6961988644ebaa84f4
Andrew Beers <apbeers@gmail.com>
2019-03-15 17:11:36 -0500

    work on stuff

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ the results in JSON format.
 '''
 
 # Import libraries
-import numpy as np
+import socket
 from flask import Flask, request, jsonify
 import pickle
 import os
@@ -26,8 +26,23 @@ vectorizer = pickle.load(open(vectorizer_path, 'rb'))
 
 @app.route('/api', methods=['POST'])
 def predict():
+
     # Get the data from the POST request.
     data = request.get_json(force=True)
+
+    if not data['ip_for_url']:
+        try:
+            data['ip_for_url'] = socket.gethostbyname(data['url'])
+        except socket.gaierror as e:
+            print('Error looking up url: {0}'.format(e))
+
+    if data['white_listed_ips'] and data['ip_for_url'] in data['white_listed_ips']:
+        return jsonify('IP for url is whitelisted: {0}'.format(data['ip_for_url']))
+
+    if data['black_listed_ips'] and data['ip_for_url'] in data['black_listed_ips']:
+        return jsonify('IP for url is blacklisted {0}'.format(data['ip_for_url']))
+
+    print(data)
 
     # Make prediction using model loaded from disk as per the data.
     prediction = model.predict(vectorizer.transform([data['url']]))
@@ -37,7 +52,7 @@ def predict():
 
 @app.route('/')
 def hello_world():
-    return "Hello, World!"
+    return "Hey there! You may want to try hitting the API instead of this URL. More info can be found at linked in"
 
 
 if __name__ == '__main__':
